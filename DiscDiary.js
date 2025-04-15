e211e1723a9aa7d39f836cbcb5271afa6c7515c1
John Doe <johndoe@example.com>
2025-04-15 10:01:49 +0200

    version1.1

---
+++ DiscDiary.js
@@ -0,0 +1,17 @@
+let p = document.querySelector("p");
+console.log(p);
+
+let div = document.getElementById("total_entries");
+let forumsText = "Total Entries:";
+div.textContent = "7";
+
+let div = document.getElementById("year_range");
+let forumsText = "Year Range";
+div.textContent = "";
+
+let div = document.getElementById("top_artist");
+let forumsText = "Top Artist:";
+div.textContent = "me";
+
+let button = document.getElementById("button")
+button.addEventListener("click")
